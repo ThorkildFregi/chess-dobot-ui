7b21da9bcb0954b2c7f05b702fa07ed3310114e6
ThorkildFregi <gabrielbermesp@gmail.com>
2025-10-02 20:12:11 +0200

    Link API and paying on board

--- app/app.tsx
+++ app/app.tsx
@@ -1,11 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Chessboard, { ChessboardRef } from 'react-native-chessboard';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
     const ref = useRef<ChessboardRef>(null);
-    const response = fetch("http://192.168.0.107:8081/").then(response => {console.log(response)});
+    const code = 0;
+    
+    const [pendingMove, setPendingMove] = useState<{ from: string; to: string } | null>(null);
+
+    useEffect(() => {
+        if (pendingMove && ref.current) {
+            ref.current.move({ from: pendingMove.from, to: pendingMove.to });
+            setPendingMove(null);
+        }
+    }, [pendingMove]);
 
     return (
         <View style={styles.container}>
@@ -14,9 +23,33 @@ export default function App() {
                     ref={ref}
                     withLetters={true}
                     withNumbers={true}
+                    gestureEnabled={true}
                     onMove={({ state }) => {
-                        console.log(state.fen);
-                        console.log(response);
+                        const fen = state.fen;
+                        const split = fen.split(" ");
+                        if (split[1] == "b") {
+                            const request = new XMLHttpRequest();
+                                request.onreadystatechange = e => {
+                                if (request.readyState !== 4) {
+                                    return;
+                                }
+
+                                if (request.status === 202) {
+                                    const move = request.responseText;
+
+                                    const from = move.substring(0, 2);
+                                    const to = move.substring(2, move.length);
+                                    setPendingMove({ from, to });
+
+                                    console.log('success', request.responseText);
+                                } else {
+                                    console.warn('error');
+                                }
+                            };
+
+                            request.open("get", `http://192.168.0.107:8081/makeamove?code=${code}&fen=${fen}`);
+                            request.send()                          
+                        }
                     }}
                 />
             </GestureHandlerRootView>
