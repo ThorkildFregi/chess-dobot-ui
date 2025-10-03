27f818fb9ec576b21a5f75389c684c38b007d284
ThorkildFregi <gabrielbermesp@gmail.com>
2025-10-03 13:30:18 +0200

    Add navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,58 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Chessboard, { ChessboardRef } from 'react-native-chessboard';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useRouter } from 'expo-router';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    const ref = useRef<ChessboardRef>(null);
-    const code = 0;
-    
-    const [pendingMove, setPendingMove] = useState<{ from: string; to: string } | null>(null);
-
-    useEffect(() => {
-        if (pendingMove && ref.current) {
-            ref.current.move({ from: pendingMove.from, to: pendingMove.to });
-            setPendingMove(null);
-        }
-    }, [pendingMove]);
+export default function Index() {
+    const router = useRouter();
 
     return (
         <View style={styles.container}>
-            <GestureHandlerRootView style={styles.gesturehandler}>
-                <Chessboard 
-                    ref={ref}
-                    withLetters={true}
-                    withNumbers={true}
-                    gestureEnabled={true}
-                    onMove={({ state }) => {
-                        const fen = state.fen;
-                        const split = fen.split(" ");
-                        if (split[1] == "b") {
-                            const request = new XMLHttpRequest();
-                                request.onreadystatechange = e => {
-                                if (request.readyState !== 4) {
-                                    return;
-                                }
-
-                                if (request.status === 202) {
-                                    const move = request.responseText;
-
-                                    const from = move.substring(0, 2);
-                                    const to = move.substring(2, move.length);
-                                    setPendingMove({ from, to });
-
-                                    console.log('success', request.responseText);
-                                } else {
-                                    console.warn('error');
-                                }
-                            };
-
-                            request.open("get", `http://192.168.232.77:8081/makeamove?code=${code}&fen=${fen}`);
-                            request.send()                          
-                        }
-                    }}
-                />
-            </GestureHandlerRootView>
+        <Text style={styles.text}>Home screen</Text>
+        <Button title="Play" onPress={() => router.navigate('/play')} />
         </View>
     );
 }
@@ -64,9 +19,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    gesturehandler: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+    text: {
+        color: '#fff',
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#fff',
+    },
 });
---
+++ app/play.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Chessboard, { ChessboardRef } from 'react-native-chessboard';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function Play() {
+    const ref = useRef<ChessboardRef>(null);
+    const code = 0;
+    
+    const [pendingMove, setPendingMove] = useState<{ from: string; to: string } | null>(null);
+
+    useEffect(() => {
+        if (pendingMove && ref.current) {
+            ref.current.move({ from: pendingMove.from, to: pendingMove.to });
+            setPendingMove(null);
+        }
+    }, [pendingMove]);
+
+    return (
+        <View style={styles.container}>
+            <GestureHandlerRootView style={styles.gesturehandler}>
+                <Chessboard 
+                    ref={ref}
+                    withLetters={true}
+                    withNumbers={true}
+                    gestureEnabled={true}
+                    onMove={({ state }) => {
+                        const fen = state.fen;
+                        const split = fen.split(" ");
+                        if (split[1] == "b") {
+                            const request = new XMLHttpRequest();
+                                request.onreadystatechange = e => {
+                                if (request.readyState !== 4) {
+                                    return;
+                                }
+
+                                if (request.status === 202) {
+                                    const move = request.responseText;
+
+                                    const from = move.substring(0, 2);
+                                    const to = move.substring(2, move.length);
+                                    setPendingMove({ from, to });
+
+                                    console.log('success', request.responseText);
+                                } else {
+                                    console.warn('error');
+                                }
+                            };
+
+                            request.open("get", `http://192.168.232.77:8081/makeamove?code=${code}&fen=${fen}`);
+                            request.send()                          
+                        }
+                    }}
+                />
+            </GestureHandlerRootView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#111',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    gesturehandler: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
