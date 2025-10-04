bf4b887b00420939e7addcec12cad16c02a59e9d
ThorkildFregi <gabrielbermesp@gmail.com>
2025-10-04 11:20:52 +0200

    Change icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,26 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-        headerStyle: {
-          backgroundColor: '#262626',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-        <Stack.Screen name="index" options={{ headerShown: false}} />
-        <Stack.Screen name="play" options={{ headerShown: false }} />
-        <Stack.Screen name="createaparty" options={{ title: "" }} />
-        <Stack.Screen name="waiting" options={{ headerShown: false }} />
-        <Stack.Screen name="join" options={{ title: "" }} />
-        <Stack.Screen name="endparty" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack screenOptions={{
+          headerStyle: {
+            backgroundColor: '#262626',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+          <Stack.Screen name="index" options={{ headerShown: false}} />
+          <Stack.Screen name="play" options={{ headerShown: false }} />
+          <Stack.Screen name="createaparty" options={{ title: "" }} />
+          <Stack.Screen name="waiting" options={{ headerShown: false }} />
+          <Stack.Screen name="join" options={{ title: "" }} />
+          <Stack.Screen name="endparty" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
